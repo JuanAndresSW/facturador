41a672ee03bf5926a01cfd90fb7cb505c9bf8838
Marcos Lopez <lopezikaro16@gmail.com>
2022-04-17 18:31:01 -0300

    i dont know

--- facturador-demo/src/main/java/dev/facturador/branch/domain/BranchAddress.java
+++ facturador-demo/src/main/java/dev/facturador/branch/domain/BranchAddress.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -20,7 +21,7 @@ public class BranchAddress {
     private String postalCode;
     @NotEmpty
     private String street;
-    @NotEmpty
+    @NotNull
     private int numberAddress;
 
 }
--- facturador-demo/src/main/java/dev/facturador/branch/domain/BranchCreate.java
+++ facturador-demo/src/main/java/dev/facturador/branch/domain/BranchCreate.java
@@ -1,5 +1,6 @@
 package dev.facturador.branch.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +9,18 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public final class BranchCreate {
+public final class BranchCreate implements Serializable {
+    public static final Long serialVersinUID = 1L;
+
+    @JsonProperty(value = "IDTrader")
     @NotNull
     private long IDTrader;
+
     @Length(min = 3, max = 30)
     @NotEmpty
     private String name;
